b85d1fea4251f47d588ea2410de2289e5b856669
Chris <chris@modl.ai>
2022-01-29 15:07:36 +0100

    Tests

--- Assets/Examples/IK/CCDIK/CCDIKJoint.cs
+++ Assets/Examples/IK/CCDIK/CCDIKJoint.cs
@@ -3,16 +3,45 @@
 public class CCDIKJoint : MonoBehaviour {
   public Vector3 axis = Vector3.right;
   public float maxAngle = 180;
-  Vector3 perpendicular; void Start() { perpendicular = axis.Perpendicular(); }
+  Vector3 perpendicular; 
+  public Transform overrideParent;
+
+  [Header("Debug")]
+  public Vector3 baseRot;
+  public Vector3 hingeRot;
+  public Vector3 limitRot;
+  public Vector3 normal;
+  public Vector3 parentRotDebug;
+  public bool rotateToDirection;
+
+  void Start()
+  {
+      perpendicular = axis.Perpendicular();
+      if (overrideParent is null)
+          overrideParent = transform.parent;
+  }
 
   public void Evaluate(Transform ToolTip, Transform Target, bool rotateToDirection = false) {
+    var t = transform;
+    var rot = t.rotation;
+    var pos = t.position;
+    var parentRot = overrideParent.rotation;
+    parentRotDebug = parentRot.eulerAngles;
+    this.rotateToDirection = rotateToDirection;
+    
     //Rotate the assembly so the tooltip better matches the target position/direction
-    transform.rotation = (rotateToDirection ? Quaternion.FromToRotation(ToolTip.up, Target.forward) : Quaternion.FromToRotation(ToolTip.position - transform.position, Target.position - transform.position)) * transform.rotation;
-
+    rot = (rotateToDirection ? Quaternion.FromToRotation(ToolTip.up, Target.forward) : Quaternion.FromToRotation(ToolTip.position - pos, Target.position - pos)) * rot;
+    baseRot = rot.eulerAngles;
+    
     //Enforce only rotating with the hinge
-    transform.rotation = Quaternion.FromToRotation(transform.rotation * axis, transform.parent.rotation * axis) * transform.rotation;
+    rot = Quaternion.FromToRotation(rot * axis, parentRot * axis) * rot;
+    hingeRot = rot.eulerAngles;
 
     //Enforce Joint Limits
-    transform.rotation = Quaternion.FromToRotation(transform.rotation * perpendicular, (transform.rotation * perpendicular).ConstrainToNormal(transform.parent.rotation * perpendicular, maxAngle)) * transform.rotation;
+    rot = Quaternion.FromToRotation(rot * perpendicular, (rot * perpendicular).ConstrainToNormal(parentRot * perpendicular, maxAngle)) * rot;
+    limitRot = rot.eulerAngles;
+    normal = parentRot * perpendicular;
+
+    t.rotation = rot;
   }
 }
